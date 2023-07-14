f3fe410c80cdc9693e64aa942b0e6d128b98cc3c
TikhonovMikhail <tixonovmisha@gmail.com>
2023-07-14 20:32:56 +0400

    test project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
+import { fa1, fa2, fa3, faQuestion, fas } from '@fortawesome/free-solid-svg-icons';
 import { Component } from '@angular/core';
-
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent {constructor(library: FaIconLibrary) {
+  library.addIconPacks(fas);
+}
+
+  title = 'AngularTest';
+  iconNumber=-1;
+  iconsArray=[faQuestion,faQuestion,faQuestion,faQuestion,faQuestion]
+  delay(ms: number) {
+  return new Promise( resolve => setTimeout(resolve, ms) );
+    }
+ async getRandomIcon(){
+  let keys=Object.keys(fas);
+  keys[ keys.length * Math.random() << 0]
+    this.iconNumber++;
+    if(this.iconNumber>4)this.iconNumber=0;
+    let localIconNumber= this.iconNumber;
+    this.iconsArray[localIconNumber]=fa3;
+    await this.delay(1000);
+    this.iconsArray[localIconNumber]=fa2;
+    await this.delay(1000);
+    this.iconsArray[localIconNumber]=fa1;
+    await this.delay(1000);
+    this.iconsArray[localIconNumber]=fas[keys[ keys.length * Math.random() << 0]]
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
